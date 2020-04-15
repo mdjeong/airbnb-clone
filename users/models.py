0dfac214a017c98a4c97a639160533368d1d2d91
정현진 <chcin@naver.com>
2020-04-15 16:46:56 +0900

    update new models

--- users/models.py
+++ users/models.py
@@ -1,6 +1,27 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
 class User(AbstractUser):
-    bio = models.TextField(default="")
+
+    """  Custom User Model """
+
+    GENDER_MALE = "male"
+    GENDER_FEMALE = "female"
+    GENDER_CHOICES = ((GENDER_MALE, "Male"), (GENDER_FEMALE, "Female"))
+
+    LANG_ENGLISH = "en"
+    LANG_KOREAN = "kr"
+    LANG_CHOICES = ((LANG_ENGLISH, "English"), (LANG_KOREAN, "한국어"))
+
+    CURRENCY_USD = "usd"
+    CURRENCY_KRW = "krw"
+    CURRENCY_CHOICES = ((CURRENCY_USD, "USD"), (CURRENCY_KRW, "KRW"))
+
+    avatar = models.ImageField(blank=True)
+    gender = models.CharField(choices=GENDER_CHOICES, max_length=10, blank=True)
+    bio = models.TextField(default="", blank=True)
+    birthdate = models.DateField(blank=True, null=True)
+    language = models.CharField(choices=LANG_CHOICES, max_length=12, blank=True)
+    currency = models.CharField(choices=CURRENCY_CHOICES, max_length=3, blank=True)
+    superhost = models.BooleanField(default=False)
