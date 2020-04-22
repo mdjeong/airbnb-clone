e917aa822eb4370fe0b7d4c295f6d8273f560741
정현진 <chcin@naver.com>
2020-04-22 12:11:18 +0900

    list view

--- rooms/views.py
+++ rooms/views.py
@@ -1,13 +1,25 @@
-from math import ceil
-from django.shortcuts import render
-from django.core.paginator import Paginator
+from django.shortcuts import render, redirect
+from django.core.paginator import Paginator, EmptyPage
+from django.views.generic import ListView
 from . import models
+
+
+class HomeView(ListView):
+
+    """ HomeView Class Definition """
+
+    model = models.Room
+    paginate_by = 10
+    paginate_orphans = 5
+    ordering = "created"
 
 
 def all_rooms(request):
     page = request.GET.get("page", 1)
-    room_list = models.Room.objects.all()
-    paginator = Paginator(room_list, 10)
-    rooms = paginator.get_page(page)
-    print(vars(rooms.paginator))
-    return render(request, "rooms/home.html", {"rooms": rooms})
+    room_list = models.Room.objects.all()  # queryset
+    paginator = Paginator(room_list, 10, orphans=5)
+    try:
+        rooms = paginator.page(int(page))
+        return render(request, "rooms/home.html", {"pages": rooms})
+    except EmptyPage:
+        return redirect("/")
